55dbd82858239e4b8e83f76066da28e70f0b1128
Michael Reilly <OmnipotentEntity@gmail.com>
2014-05-26 12:47:19 +0100

    Some SDL2 initialization

--- src/main.hs
+++ src/main.hs
@@ -1,2 +1,23 @@
+import qualified Graphics.UI.SDL as SDL
+import Foreign.C.String
+import Foreign.C.Types
+
 main :: IO ()
-main = putStrLn "Buttface"
+main = do
+  initialize
+  mainLoop
+
+mkCInt :: Int -> CInt
+mkCInt n = fromIntegral n
+
+initialize :: IO ()
+initialize =
+  do
+    _ <- SDL.init(SDL.initFlagEverything)
+    sdlWindow <- withCString "Test" $ \title ->
+      SDL.createWindow title SDL.windowPosCentered SDL.windowPosCentered (mkCInt 640) (mkCInt 480) SDL.windowFlagOpenGL
+    sdlRenderer <- SDL.createRenderer sdlWindow (mkCInt $ -1) SDL.rendererFlagAccelerated
+    putStr "Butts\n"
+
+mainLoop :: IO ()
+mainLoop = putStr "Butts\n"
