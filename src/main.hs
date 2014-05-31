628e688d4aae0bdd0d9fc655614f6eea92c3d107
kyren <kerriganw@gmail.com>
2014-05-31 22:17:39 +0100

    That's better

--- src/main.hs
+++ src/main.hs
@@ -2,11 +2,6 @@ import qualified Graphics.UI.SDL as SDL
 import Foreign.C.String
 import Foreign.C.Types
 
-main :: IO ()
-main = do
-  initialize
-  mainLoop
-
 mkCInt :: Int -> CInt
 mkCInt = fromIntegral
 
@@ -17,10 +12,11 @@ initialize =
     sdlWindow <- withCString "Test" $ \title ->
       SDL.createWindow title SDL.windowPosCentered SDL.windowPosCentered (mkCInt 640) (mkCInt 480) SDL.windowFlagOpenGL
     _ <- SDL.createRenderer sdlWindow (mkCInt (-1)) SDL.rendererFlagAccelerated
-    putStr "Butts\n"
+    return ()
 
-mainLoop :: IO ()
-mainLoop = do
-  putStr "LBL 10\n"
-  putStr "GOTO 10\n"
-  mainLoop
+main :: IO ()
+main = do
+    initialize
+    putStrLn ">10 PRINT \"LOOK AROUND YOU\""
+    putStrLn ">20 GOTO 10"
+    sequence_ $ repeat $ putStr "LOOK AROUND YOU"
