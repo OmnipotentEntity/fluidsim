d34f8ca74b0e53315d8ecd1162667aec44e53522
kyren <kerriganw@gmail.com>
2014-05-31 21:46:10 +0100

    Hey, a working SDL window on os x without hax

--- src/main.hs
+++ src/main.hs
@@ -8,16 +8,19 @@ main = do
   mainLoop
 
 mkCInt :: Int -> CInt
-mkCInt n = fromIntegral n
+mkCInt = fromIntegral
 
 initialize :: IO ()
 initialize =
   do
-    _ <- SDL.init(SDL.initFlagEverything)
+    _ <- SDL.init SDL.initFlagEverything
     sdlWindow <- withCString "Test" $ \title ->
       SDL.createWindow title SDL.windowPosCentered SDL.windowPosCentered (mkCInt 640) (mkCInt 480) SDL.windowFlagOpenGL
-    sdlRenderer <- SDL.createRenderer sdlWindow (mkCInt $ -1) SDL.rendererFlagAccelerated
+    _ <- SDL.createRenderer sdlWindow (mkCInt (-1)) SDL.rendererFlagAccelerated
     putStr "Butts\n"
 
 mainLoop :: IO ()
-mainLoop = putStr "Butts\n"
+mainLoop = do
+  putStr "LBL 10\n"
+  putStr "GOTO 10\n"
+  mainLoop
